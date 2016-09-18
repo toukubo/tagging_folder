9568db8b30b0475fd10f5e85ba4efa502538424a
TOKUBOKOJI <toukubo@gmail.com>
2016-09-18 21:22:13 +0900

    working commit initially

---
+++ tag.py
@@ -0,0 +1,24 @@
+import os
+from os.path import join
+
+
+def ourrename(tagString,file):
+    if tagString in file:
+        return
+    basedir = tagString + "/"
+    taggedFiles = os.rename(basedir + file, basedir + file + " " + tagString)
+
+class Tagging():
+    def __init__(self,tagString):
+        if not tagString.startswith("#"):
+            return
+        files = os.listdir(tagString)
+        for file in files:
+            ourrename(tagString,file)
+class TaggingAllFolderContent():
+    def __init__(self):
+        tagDirs = os.listdir(".")
+        for dir in tagDirs:
+            Tagging(dir)
+if __name__ == '__main__':
+    TaggingAllFolderContent()
